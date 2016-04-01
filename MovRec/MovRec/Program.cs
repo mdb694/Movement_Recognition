e04d8f6f14a967781533bc09b9ab801a7c96352c
mdb694 <mdb69@hotmail.it>
2016-04-01 18:45:32 +0200

    Implementata la socket lato server per la ricezione dei dati dal simulatore

---
+++ MovRec/MovRec/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace MovRec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var EP = new IPEndPoint(IPAddress.Any, 45555);
+
+            // Creo una socket di tipo TCP
+            var listener = new TcpListener(EP);
+            listener.Start();
+
+            var socket = listener.AcceptSocket(); // blocca
+        }
+    }
+}
